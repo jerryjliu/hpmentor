14a6850b91baa7337c4a9e6a4e9295b17328f524
Jerry Liu <jerryjliu98@gmail.com>
2014-11-02 21:19:56 -0500

    first commit of mentorship page

---
+++ client/hpmentor/hpmentor.js
@@ -0,0 +1,31 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault("counter", 0);
+
+  Template.hpmentor.helpers({
+    counter: function () {
+      return Session.get("counter");
+    },
+    requests: function () {
+      return Requests.find();
+    }
+  });
+
+  Template.hpmentor.events({
+    'click button': function(event) {
+      var id = event.currentTarget.id;
+      var requests = Requests.find({_id: id}).fetch();
+      var request = requests[0];
+      if (request.statusg) {
+        Requests.update({_id: id}, {$set: {statusg: null, statusy: true}});
+      }
+      else if (request.statusy) {
+        Requests.update({_id: id}, {$set: {statusy: null, statusr: true}});
+      }
+    }
+  });
+        
+    Meteor.startup(function() {
+        $('html').attr('style', 'background-color: #f5f5f5');
+    });
+}
