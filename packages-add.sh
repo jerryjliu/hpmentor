6e9272185d7368b8db422dc1a886031870ee7996
Katherine Lee <kate.lee168@gmail.com>
2014-11-14 17:11:01 -0500

    updated packages

---
+++ packages-add.sh
@@ -0,0 +1,11 @@
+# Meteor packages used by this project, one per line.
+#
+# 'meteor add' and 'meteor remove' will edit this file for you,
+# but you can also edit it by hand.
+
+meteor add meteor-platform
+meteor add autopublish
+meteor add insecure
+meteor add iron:router
+meteor add taggingjs
+
